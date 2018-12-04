7639c98c92fc91e11fa7a21cc74973fcf7de07c8
Paweł Makowski <m4kpaul@gmail.com>
2018-12-04 05:39:41 +0100

    Added fixed scanning test app as a base

---
+++ DesktopApp/DesktopApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ModernWpf;
+using System.Windows;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            UIHooks.EnableHighDpiSupport();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # desktop-app
+
+Library: https://github.com/soukoku/ntwain/tree/v3
+Twain docs: https://twain.org/wp-content/uploads/2017/03/TWAIN-2.4-Specification.pdf
